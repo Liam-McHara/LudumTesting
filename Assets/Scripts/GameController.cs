48a71a6bdc1a4afcfdc1c8030c4eb5afa105e3a7
Liam-McHara <guillemlr@pm.me>
2020-04-19 19:11:30 +0200

    testig parcial de AVENUE

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -13,7 +13,7 @@ public class GameController : MonoBehaviour
     public int fase;    // fase del día (1 - 5)
     public int loop = 1;
 
-    public string continueText = "Continue";
+    public string continueText = ">";
 
     /*  0 - mapa
      *  1 - casa
@@ -103,18 +103,17 @@ public class GameController : MonoBehaviour
         lastText = "001";   //    < 001 >
         // Prepara el text
         string str;
-        if (!v7_001) str = "Me he dirigido a la avenida principal de la ciudad en busca de nuevas pistas. Todo el mundo pasa por la avenida en algún momento, de modo que, lógicamente, el asesino también se encontrará aqui tarde o temprano. Nada más llegar a la avenida, me ha llamado la atención…";
+        if (!v7_001) str = "My search for new clues led me to the main avenue of the town. Everyone passes through here at some point, so I figured that, logically, the assassin would do so too. As soon as I came to the avenue, my attention was caught by...";
         else
         {
             v7_001 = true;
-            str = "He decidido regresar de nuevo a la avenida. Aún me quedan cosas por hacer aquí, estoy seguro. Nada más llegar, me he dirigido hacia...";
+            str = "I came back to the Avenue. I could feel there was something left for me to do there. As soon as I got there, I headed to…";
         }
         // Prepara les opcions...
-        string op1 = "un callejón de aspecto sospechoso";
-        string op2 = "una niña de aspecto inocente";
-        string op3 = "una tienda de chocolates";
+        string op1 = "...a suspicious alleway...";
+        string op2 = "...an innocent looking girl...";
+        string op3 = "...a chocolate store...";
         pm.UpdateOptions(op1, op2, op3);    // ...i les actualitza
-
         // Mostra el panell amb text i opcions corresponents
         pm.ShowPanel(str);
 
@@ -146,8 +145,7 @@ public class GameController : MonoBehaviour
                 if (lastText == "001")
                 {
                     lastText = "002";       // < 002 >
-                    string str = "a suspicious alleway. Si yo fuera un asesino de girasoles, sin duda esa es la clase de sitio en el que me escondería a maquinar planes malvados. \nEl callejón estaba desierto a excepción de un perro pulgoso y unas cuantas botellas rotas.Estaba a punto de marcharme, decepcionado, cuando de pronto he escuchado una voz misteriosa que parecía salir de ninguna parte. “Pssst, you!Yeah, you!Do you know how many times the sun rose today ?”";
-
+                    string str = "a suspicious alleway. If I was a murderer of sunflowers, that’s clearly kind of play where I would hide to plot my evil plans. \nThe alleway was empty except for a dirty dog and some broken bottles. I was about to leave when I heard a mysterious voice that seemed to come from nowhere in particular.  \n“Pssst, you! Yeah, you! Do you know how many times the sun rose today?”";
                     pm.UpdateOptions(1);
                     pm.UpdateText(str);
                 }
@@ -158,7 +156,7 @@ public class GameController : MonoBehaviour
                     pm.UpdateOptions(continueText);
                     pm.UpdateText(str);
                 }
-                else if (lastText == "003")
+                else if (lastText == "003" | lastText == "005" | lastText == "006" | lastText == "007")
                 {
                     lastText = "008";   //      < 008 > 
                     string str = "After that came only silence, and I was left alone feeling quite unsure of what exactly had happened.";
@@ -168,9 +166,11 @@ public class GameController : MonoBehaviour
                 else if (lastText == "004")
                 {
                     lastText = "006";    //      < 006 >
-                    string str = "“I’d say you’re just an idiot hiding under a sewer door…” I replied, losing my temper. “Brother, you’re not really angry with me, but with yourself. Sunflowers, grant patience to this lost lamb. He’ll be back once he’s ready to hear the Truth”.";
-
+                    string str = "“I’d say you’re just an idiot hiding under a sewer door…” I replied, losing my temper. \n“Brother, you’re not really angry with me, but with yourself. Sunflowers, grant patience to this lost lamb. He’ll be back once he’s ready to hear the Truth”.";
+                    pm.UpdateOptions(continueText);
+                    pm.UpdateText(str);
                 }
+
                 else if (lastText == "008") GotoMapa(); 
                 break;
             default:
@@ -199,13 +199,20 @@ public class GameController : MonoBehaviour
                 }
                 else if (lastText == "002b")
                 {
-                    lastText = "004";      
-
+                    lastText = "004";      //    < 004 > 
+                    string str = "“And who the hell are you?”, I asked quite briskly. I admit I don’t take too kindly to invisible strangers startling me in small alleways. \n“Worry not, sunflower Brother. As I said, I am the Mysterious Voice™, Protector of the Poor and Helpless, Saver of Worlds, and Professional Adventure Advisor.”";
+                    string op1, op2;
+                    op1 = "“I’d say you’re just an idiot hiding under a sewer door…”";
+                    op2 = "“If you say so…”";
+                    pm.UpdateOptions(op1, op2);
+                    pm.UpdateText(str);
                 }
                 else if (lastText == "004")
                 {
-                    lastText = "007";     
-
+                    lastText = "007";      //    < 007 >
+                    string str = "“If you say so…”, I said, hesitating. \n“Of course I say so”, replied the Voice with unwavering confidence. “Now go save a space-time continuum or two. And remember me when you find yourself in need of a friendly, mysterious Voice.”";
+                    pm.UpdateOptions(continueText);
+                    pm.UpdateText(str);
                 }
                 break;
             default:
@@ -249,14 +256,14 @@ public class GameController : MonoBehaviour
             case 7:     // PASSEIG
                 if (lastText == "002")
                 {
-                    lastText = "002b";
+                    lastText = "002b";      //  < 002b >
                     // Prepara el nou text
                     string s1, s2, str;
                     if (ans == loop.ToString())
                         s1 = "The voice chuckled to itself. “Indeed, brother. Indeed. Anyway,";
                     else
                         s1 = "“Oh, dear, so innocent. It rose " + loop.ToString() + " times already. Anyway,";
-                    s2 = "you look kind of down. Seems like you might need some advise from your friendly next door mysterious voice, amirite?”";
+                    s2 = "you look kind of down. Seems like you might need some advise from your friendly next door mysterious voice, amirite?”\nAt first I thought I was imagining voices again, but this one seemed too real to be a figment. In fact, as I listened I realized the sound seemed to be coming from the sewer door directly under my feet. ";
                     str = s1 + s2;
                     // Prepara les noves opcions
                     string op1, op2;
