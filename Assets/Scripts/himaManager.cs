90dc49617651c2cc5f5ddf66365a34a55dbe8406
Liam-McHara <guillemlr@pm.me>
2020-04-21 05:14:27 +0200

    BUILD 3

--- Assets/Scripts/himaManager.cs
+++ Assets/Scripts/himaManager.cs
@@ -8,31 +8,38 @@ public class himaManager : MonoBehaviour
     public Sprite hima1, hima2, hima3, hima4, hima5;
     GameController gc;
     Image img;
+    Text faseText;
 
     void Start()
     {
         gc = FindObjectOfType<GameController>();
         img = GetComponent<Image>();
+        faseText = GetComponentInChildren<Text>();
     }
 
     void Update()
     {
         switch (gc.t)
-        {
+        { 
             case 1:
                 img.sprite = hima1;
+                faseText.text = "dawn";
                 break;
             case 2:
                 img.sprite = hima2;
+                faseText.text = "morning";
                 break;
             case 3:
                 img.sprite = hima3;
+                faseText.text = "noon";
                 break;
             case 4:
                 img.sprite = hima4;
+                faseText.text = "afternoon";
                 break;
             case 5:
                 img.sprite = hima5;
+                faseText.text = "dusk";
                 break;
         }
     }
