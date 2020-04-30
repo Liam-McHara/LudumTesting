74087382bfd1a7e1714ba1c66439d0be74dba41b
Liam_McHara <guillemlr@pm.me>
2020-04-30 14:02:29 +0200

    GameAssets Object

---
+++ Assets/GameAssets.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    private static GameAssets _i;
+
+    public static GameAssets i
+    {
+        get
+        {
+            if (_i == null) _i = Instantiate(Resources.Load<GameAssets>("GameAssets"));
+            return _i;
+        }
+    }
+
+    public AudioClip click;
+    public AudioClip openingMusic;
+    public AudioClip endingMusic;
+    public AudioClip victoryMusic;
+    public AudioClip mapaMusic;
+    public AudioClip homeMusic;
+    public AudioClip dumpsterMusic;
+    public AudioClip wharehouseMusic;
+    public AudioClip parkMusic;
+    public AudioClip waterplantMusic;
+    public AudioClip schoolMusic;
+    public AudioClip pizzaMusic;
+    public AudioClip avenueMusic;
+}
