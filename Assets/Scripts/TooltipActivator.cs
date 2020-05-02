7a7d1508272697a9fd2029ab3c4b813bc0863082
Liam_McHara <guillemlr@pm.me>
2020-05-02 23:01:30 +0200

    Mejora de la implementación de tooltip

---
+++ Assets/Scripts/TooltipActivator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class TooltipActivator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public string TooltipText = "Default text";
+    Tooltip tooltip;
+
+    void Awake()
+    {
+        tooltip = FindObjectOfType<Tooltip>().GetComponent<Tooltip>();
+    }
+
+    // PointerEventData eventData
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltip.ShowTooltip(TooltipText);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.HideTooltip();
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tooltip.HideTooltip();
+    }
+
+}
